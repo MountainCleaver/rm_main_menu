23dcfcc23441189a0a2b7cb5a8d83dbf06401f7b
MountainCleaver <juliusromero009988@gmail.com>
2025-07-28 17:08:27 +0800

    sample screens after menu

--- mainMenu.gd
+++ mainMenu.gd
@@ -6,24 +6,50 @@ var currentIndex = 0;
 var selection : Array;
 
 func _ready() -> void:
-	selection = main_menu_options.get_children();
+	selection = main_menu_options.get_children()
+	for node in selection:
+		node.focus_mode = Control.FOCUS_CLICK
 	selection[currentIndex].grab_focus()
 
-func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("btn_down"):
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_down"):
 		if currentIndex == (selection.size())-1:
 			currentIndex = 0;
 		else:
 			currentIndex += 1;
 		print(currentIndex);
 		selection[currentIndex].grab_focus()
-	elif event.is_action_pressed("btn_up"):
+		get_viewport().set_input_as_handled();
+		
+	elif event.is_action_pressed("ui_up"):
 		if currentIndex == 0:
 			currentIndex = (selection.size())-1;
 		else:
 			currentIndex -= 1;
 		print(currentIndex);
 		selection[currentIndex].grab_focus()
+		get_viewport().set_input_as_handled();
+		
 	
-	if event.is_action_pressed("ui_accept"):
+	if event.is_action_pressed("accept"):
 		print(selection[currentIndex].text, " : ", currentIndex);
+		
+		match selection[currentIndex].text:
+			"Continue":
+				get_tree().change_scene_to_file("res://continue_screen.tscn")
+			"New Game":
+				get_tree().change_scene_to_file("res://new_game.tscn")
+			"Report A Bug":
+				get_tree().change_scene_to_file("res://report.tscn")
+			"Credits":
+				get_tree().change_scene_to_file("res://credits.tscn")
+			"Options":
+				get_tree().change_scene_to_file("res://options.tscn")
+			"Exit":
+				get_tree().quit();
+		#get_viewport().set_input_as_handled();
+	
+	if event.is_action_pressed("back"):
+		get_tree().quit();
+		get_viewport().set_input_as_handled();
